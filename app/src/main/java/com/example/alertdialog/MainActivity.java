47f1e56757e706339cbdafbbe39e3bb5d436b969
shivaram0005 <shivaram8534@gmail.com>
2020-11-08 13:00:55 +0530

    AlertDialog in Android

---
+++ app/src/main/java/com/example/alertdialog/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.alertdialog;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Alert");
+                builder.setMessage("Are you sure you want to exit");
+                builder.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+                builder.setNegativeButton("no",null);
+                AlertDialog alertDialog=builder.create();
+                alertDialog.show();
+
+            }
+        });
+    }
+}
